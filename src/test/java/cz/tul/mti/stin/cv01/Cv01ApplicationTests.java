dfc767a71778355ffb4682559426f3dfe77f854c
Jan Skrbek <jan.skrbek1@tul.cz>
2025-02-13 15:08:19 +0100

    Added cv01 spring

---
+++ src/test/java/cz/tul/mti/stin/cv01/Cv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.mti.stin.cv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
